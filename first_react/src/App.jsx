e6c647f6226ff6b3c123a7ac3919ff3d04f9c900
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-19 20:11:38 +0530

    First React Application
    Created component in src, exported module

---
+++ first_react/src/App.jsx
@@ -0,0 +1,5 @@
+import Greet from './components/greet'
+function App(){
+  return <Greet/>//function component
+}
+export default App
---
+++ first_react/src/components/greet.jsx
@@ -0,0 +1,5 @@
+//user defined component
+let Greet=function(){
+    return <h1>Hello, Welcome to React</h1>
+}
+export default Greet

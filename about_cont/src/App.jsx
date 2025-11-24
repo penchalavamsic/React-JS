0aed77f4e6f715f86503d6e0c21f27e0c03a87c8
Penchala Vamsi Chebrolu <69138538+penchalavamsic@users.noreply.github.com>
2025-11-24 09:35:44 +0530

    About us, contact components

---
+++ about_cont/src/App.jsx
@@ -0,0 +1,11 @@
+import About from "./components/about";
+import Contact from "./components/Contact";
+function App(){
+  return (
+    <div>
+      <About/>
+      <Contact/>
+    </div>
+  )
+}
+export default App
---
+++ about_cont/src/components/Contact.jsx
@@ -0,0 +1,14 @@
+function Contact(){
+    return(
+        <div>
+            <h1>Contact</h1>
+            <table>
+                <tr>Email:cdahyd@gmail.com</tr>
+                <tr>www.cdac.in</tr>
+                <tr>Phone:123456</tr>
+            </table>
+        </div>
+    )
+
+}
+export default Contact
